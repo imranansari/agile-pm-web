f591209f4073ac14f053ec7414e531248f5716c0
iansari <imran.iansari@gmail.com>
2011-04-02 00:14:28 -0400

    move epic update

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -62,19 +62,23 @@ $(document).ready(function() {
 
         displayEpics: function() {
             if (typeof epicModels == "undefined") {
+                //getEpicModels();
                 epicModels = new EpicModels(initData);
             }
 
             // Create view instances for every model
             epicViews = epicModels.map(function(epicModel) {
-                var view = new EpicView({model: epicModel});
+                var view = new EpicView({id: 'epic_'+epicModel.id, model: epicModel});
 
                 var phaseName = "story_" + epicModel.toJSON().phase;
+                //console.log(view.render().el);
                 $('#' + phaseName + ' ul').append(view.render().el);
                 //$('#epic_p1').append();
                 return view;
             });
         },
+
+        
 
         editEpic: function(editEpicModel) {
             console.log(editEpicModel.toJSON().phase);
@@ -110,6 +114,15 @@ $(document).ready(function() {
 
             var phaseName = "story_QA";
             $('#' + phaseName + ' ul').append(view.render().el);
+            //epicModel.save();
+        },
+
+        updateEpicLocation: function(epicsInPhase, updatedEpic){
+            var phaseId = $(updatedEpic).closest(".phasepanel").attr('id');
+            console.log(phaseId);
+            $(epicsInPhase).each(function(){
+               console.log(this);
+            });
         }
 
     });
