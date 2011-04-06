a46d495c035d6032ad2a08d615dcd9fa0481f87a
iansari <imran.iansari@gmail.com>
2011-04-05 23:25:36 -0400

    checkin

---
+++ configure_mongo.rb
@@ -0,0 +1,11 @@
+configure do
+  Mongoid.configure do |config|
+    name = "demo"
+    host = "localhost"
+    config.master = Mongo::Connection.new.db(name)
+    config.slaves = [
+        Mongo::Connection.new(host, 27017, :slave_ok => true).db(name)
+    ]
+    config.persist_in_safe_mode = false
+  end
+end
